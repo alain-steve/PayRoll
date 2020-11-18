58d3eea386cb4553c8676136fcd52729845d76b9
alain-steve <alainstevemuhirwa@yahoo.com>
2020-11-18 10:53:43 +0200

    Add project files.

---
+++ PayRoll.Persistance/Migrations/20201118052048_AddingPhoneNoToEmployeeTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PayRoll.Persistance.Migrations
+{
+    public partial class AddingPhoneNoToEmployeeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNo",
+                table: "Employees",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNo",
+                table: "Employees");
+        }
+    }
+}
